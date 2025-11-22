59dd6860cf52cab359effce9a888ef6e7b584a98
Azhits <144426639+Azhits@users.noreply.github.com>
2025-11-22 10:21:29 +0300

    Create utils package __init__.py

---
+++ genshin_autobot/utils/__init__.py
@@ -0,0 +1,12 @@
+"""Utilities package initialization.
+
+Пакет вспомогательных функций.
+
+Этот модуль содержит общие вспомогательные функции,
+декораторы и валидаторы.
+
+This module contains common utility functions,
+decorators, and validators.
+"""
+
+__all__ = []
