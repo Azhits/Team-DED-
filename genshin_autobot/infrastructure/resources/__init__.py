58acf76a0fb9d76cd8854b22d345d0c9d53ed096
Azhits <144426639+Azhits@users.noreply.github.com>
2025-11-22 10:29:05 +0300

    Create resources package __init__.py

---
+++ genshin_autobot/infrastructure/resources/__init__.py
@@ -0,0 +1,12 @@
+"""Resources package initialization.
+
+Пакет ресурсов.
+
+Этот пакет содержит статические ресурсы,
+изображения и другие файлы.
+
+This package contains static resources,
+images, and other files.
+"""
+
+__all__ = []
