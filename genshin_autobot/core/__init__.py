7e6ba74532601587e2baedc8a181bd5cf658a92f
Azhits <144426639+Azhits@users.noreply.github.com>
2025-11-22 10:14:45 +0300

    Create core package __init__.py

---
+++ genshin_autobot/core/__init__.py
@@ -0,0 +1,16 @@
+"""Core package initialization.
+
+Коровый пакет приложения.
+
+Этот модуль инициализирует основные компоненты приложения,
+включая сущности и use cases (варианты использования).
+
+This module initializes core application components,
+including entities and use cases.
+"""
+
+from genshin_autobot.core import entities
+
+__all__ = [
+    'entities',
+]
